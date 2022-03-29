abb40b0de1afc114e0652cd5b6ade5c202aebc34
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-29 10:29:30 +0200

    add parseInt to values

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const content = document.querySelector ('.js-content');
 const balance =document.querySelector ('.js-balance');
 
 //CONSTANTES DE INPUTS
-const selectDiceNum = diceNumber.value;
-const userBet =userNumber.value;
+
+
 
 // CONTANTES PARA RESULTADO TEXTO
-const win = `<h4> Has ganado el doble de lo apostado</h4>`;
-const loose = `<h4> Has perdido lo apostado</h4>`;
+const win = `<h3> Has ganado el doble de lo apostado</h3>`;
+const loose = `<h3> Has perdido lo apostado</h3>`;
 
 
 //CONTANTE PARA GUARDAR RANDOM NUMBERS
@@ -21,21 +21,22 @@ const randomNum = getRandomNumber(6);
 //FUNCIÓN NÚMERO RANDOM DADO ORDENADOR
 function getRandomNumber (max) {
     return Math.ceil(Math.random() *max);
-    
+  
 }
 
 // FUNCIÓN APUESTA
 function bet (){
     const randomNum = getRandomNumber(6);
- 
-    
+    const selectDiceNum = parseInt(diceNumber.value);
+    const userBet =parseInt(userNumber.value);
+   
     if (selectDiceNum === randomNum){
-        resultDice.innerHTML = `<p>Dado = ${randomNum} </p>`
-        content.innerHTML = win
+        resultDice.innerHTML = `<p>Dado = ${randomNum} </p>`;
+        content.innerHTML = win;
 
     }else{
-        resultDice.innerHTML = `<p>Dado = ${randomNum} </p>`
-        content.innerHTML = loose
+        resultDice.innerHTML = `<p>Dado = ${randomNum} </p>`;
+        content.innerHTML = loose;
     };
 }
 
