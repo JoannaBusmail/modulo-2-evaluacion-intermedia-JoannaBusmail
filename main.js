57e11f792552ad7c7b8429a14379b272d3329cf1
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-29 10:06:34 +0200

    add result dice - win not working

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const diceNumber = document.querySelector('.js-diceNum');
 const userNumber = document.querySelector('.js-userNum');
 const buttonPlay = document.querySelector('.js-buttonPlay');
+const resultDice = document.querySelector('.js-resultDice');
 const content = document.querySelector ('.js-content');
 const balance =document.querySelector ('.js-balance');
 
@@ -13,20 +14,27 @@ const userBet =userNumber.value;
 const win = `<h4> Has ganado el doble de lo apostado</h4>`;
 const loose = `<h4> Has perdido lo apostado</h4>`;
 
+
 //CONTANTE PARA GUARDAR RANDOM NUMBERS
 const randomNum = getRandomNumber(6);
 
 //FUNCIÓN NÚMERO RANDOM DADO ORDENADOR
 function getRandomNumber (max) {
     return Math.ceil(Math.random() *max);
+    
 }
 
 // FUNCIÓN APUESTA
 function bet (){
+    const randomNum = getRandomNumber(6);
+ 
+    
     if (selectDiceNum === randomNum){
+        resultDice.innerHTML = `<p>Dado = ${randomNum} </p>`
         content.innerHTML = win
 
     }else{
+        resultDice.innerHTML = `<p>Dado = ${randomNum} </p>`
         content.innerHTML = loose
     };
 }
@@ -34,8 +42,8 @@ function bet (){
 
 //FUNCIÓN HANDLE CLICK
 function handleClick (event){
-    event.preventDefault();
-    console.log("clickaaaa!!");
+event.preventDefault();
+  bet();
 }
 
 
