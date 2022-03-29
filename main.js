a36ff222c6100d37e70b6a5535fe7b4775fe0bac
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-29 09:32:35 +0200

    add handleClick function - working - add conditional

--- main.js
+++ main.js
@@ -6,21 +6,41 @@ const content = document.querySelector ('.js-content');
 const balance =document.querySelector ('.js-balance');
 
 //CONSTANTES DE INPUTS
-const selecDiceNum = diceNumber.value;
+const selectDiceNum = diceNumber.value;
 const userBet =userNumber.value;
 
+// CONTANTES PARA RESULTADO TEXTO
+const win = `<h4> Has ganado el doble de lo apostado</h4>`;
+const loose = `<h4> Has perdido lo apostado</h4>`;
+
+//CONTANTE PARA GUARDAR RANDOM NUMBERS
+const randomNum = getRandomNumber(6);
+
+//FUNCIÓN NÚMERO RANDOM DADO ORDENADOR
+function getRandomNumber (max) {
+    return Math.ceil(Math.random() *max);
+}
+
+// FUNCIÓN APUESTA
+function bet (){
+    if (selectDiceNum === randomNum){
+        content.innerHTML = win
+
+    }else{
+        content.innerHTML = loose
+    };
+}
 
 
-
-
-
-
-
+//FUNCIÓN HANDLE CLICK
+function handleClick (event){
+    event.preventDefault();
+    console.log("clickaaaa!!");
+}
 
 
 
 //CLICK BOTÓN
-buttonPlay.addEventListener ('click' , (event)=>{
-    event.preventDefault();
-    console.log("clickaaaa!!");
-});
+buttonPlay.addEventListener ('click' , handleClick)
+
+    
