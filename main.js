85777cdfbce09cd0e507a87b5302add08be2e058
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-29 09:17:58 +0200

    add event click button working ok

--- main.js
+++ main.js
@@ -5,4 +5,22 @@ const buttonPlay = document.querySelector('.js-buttonPlay');
 const content = document.querySelector ('.js-content');
 const balance =document.querySelector ('.js-balance');
 
+//CONSTANTES DE INPUTS
+const selecDiceNum = diceNumber.value;
+const userBet =userNumber.value;
 
+
+
+
+
+
+
+
+
+
+
+//CLICK BOTÓN
+buttonPlay.addEventListener ('click' , (event)=>{
+    event.preventDefault();
+    console.log("clickaaaa!!");
+});
