f84dadd014defbbf95b447cad2dc241b40d5e7e1
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-29 09:13:06 +0200

    add html elments to js

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-// ELEMENTOS HTML
-const diceNumber = document.querySelector
+// CONSTANTES DE ELEMENTOS HTML
+const diceNumber = document.querySelector('.js-diceNum');
+const userNumber = document.querySelector('.js-userNum');
+const buttonPlay = document.querySelector('.js-buttonPlay');
+const content = document.querySelector ('.js-content');
+const balance =document.querySelector ('.js-balance');
+
+
